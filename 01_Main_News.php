ed8a5dafb657fc2abb751a60082a4c71dc575d50
theerapat pothisawang <tonn_po@hotmail.com>
2020-08-05 21:59:16 +0700

    setup

---
+++ 01_Main_News.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once('include/header.php'); ?>
+    <?php include_once('include/style.php'); ?>
+</head>
+<body>
+    <?php include_once('include/topnav.php'); ?>
+    <div class="text-container">
+        <h2>การจัดการด้านอาหารและโภชนาการช่วง 14 วันที่ต้องอยู่บ้าน</h2>
+        <p>สถานการณ์โรคติดเชื้อไวรัสโคโรนา 2019 หรือ COVID-19 ระบาด ประชาชนควรอยู่ที่บ้าน 14 วัน เพื่อเฝ้าระวังอาการ นอกจากจะไม่ใช้ของส่วนตัวร่วมกับผู้อื่นแล้ว ต้องเลือกปรุงและกินอาหารให้เหมาะสม ถูกหลักโภชนาการ โดยกินอาหารที่มีประโยชน์หลากหลาย</p>
+    </div>
+    <!-- Content 02 -->
+    <section class="content-02">
+        <div class="container">
+            <div class="contents">
+                <div class="content">
+                    <div class="img-container">
+                        <div class="img-bg"></div>
+                        <div class="socials">
+                            <a href="#"></a>
+                            <a href="#"></a>
+                            <a href="#"></a>
+                        </div>
+                    </div>
+                    <div class="tag"></div>
+                    <div class="date"></div>
+                    <div class="text-container">
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Right side -->
+    <section class="side-right">
+
+    </section>
+
+</body>
