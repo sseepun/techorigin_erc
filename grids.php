dc75d2311daa198372ca903d47dda0ca93aaa324
Sarun Seepun <sarun_sla@hotmail.com>
2020-08-06 03:03:18 -0700

    Update Grid Styles

---
+++ grids.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once('include/header.php'); ?>
+    <?php include_once('include/style.php'); ?>
+</head>
+<body>
+    <?php include_once('include/topnav.php'); ?>
+
+    <section style="padding:2rem 0;">
+        <div class="container">
+            <div class="grids">
+                <div class="grid lg-75 md-2-3 sm-100 mt-0" style="height:200px; background:red;">
+                    Grid 1
+                </div>
+                <div class="grid lg-25 md-1-3 sm-100 md-mt-0" style="background:orange;">
+                    Grid 2
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <?php include_once('include/script.php'); ?>
+</body>
+</html>
--- include/style.php
+++ include/style.php
@@ -28,9 +28,10 @@
 <link rel="stylesheet" href="./assets/fonts/includes.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
 <link rel="stylesheet" href="./assets/lib/slick-1.8.1/slick/slick.css">
-<!-- <link rel="stylesheet" href="./assets/lib/slick-1.8.1/slick/slick-theme.css"> -->
 
 <link rel="stylesheet" href="./assets/css/style.css" />
+<link rel="stylesheet" href="./assets/css/grids.css" />
+
 <link rel="stylesheet" href="./assets/css/navs.css" />
 <link rel="stylesheet" href="./assets/css/blocks.css" />
 <link rel="stylesheet" id="css-theme" href="./assets/css/themes/0.css" />
