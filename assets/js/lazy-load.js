6c53f49f2742bb56d4bc4c04a1f7295f70ccd121
Sarun Seepun <sarun_sla@hotmail.com>
2020-08-24 05:10:34 -0700

    Update lazy load

--- assets/js/lazy-load.js
+++ assets/js/lazy-load.js
@@ -1,48 +1,45 @@
 $(function(){ 'use strict';
 
-    window.addEventListener('DOMContentLoaded', function(){
+    var lazyloadImgs = document.querySelectorAll('.lazy-img'),
+        lazyloadBgs = document.querySelectorAll('.lazy-bg'),
+        lazyloadThrottleTimeout;
+    
+    function lazyload(){
+        if(lazyloadThrottleTimeout){
+            clearTimeout(lazyloadThrottleTimeout);
+        }    
+        lazyloadThrottleTimeout = setTimeout(function(){
+            var scrollTop = window.pageYOffset;
 
-        var lazyloadImgs = document.querySelectorAll('.lazy-img'),
-            lazyloadBgs = document.querySelectorAll('.lazy-bg'),
-            lazyloadThrottleTimeout;
-        
-        function lazyload(){
-            if(lazyloadThrottleTimeout){
-                clearTimeout(lazyloadThrottleTimeout);
-            }    
-            lazyloadThrottleTimeout = setTimeout(function(){
-                var scrollTop = window.pageYOffset;
-
-                lazyloadImgs.forEach(function(img){
+            lazyloadImgs.forEach(function(img){
+                if(img.offsetTop < (window.innerHeight + scrollTop)){
                     console.log(
                         img.offsetTop,
+                        img.dataset.src,
                         window.innerHeight + scrollTop
                     )
-                    if(img.offsetTop < (window.innerHeight + scrollTop)){
-                        img.src = img.dataset.src;
-                        img.classList.remove('lazy-img');
-                    }
-                });
-                lazyloadBgs.forEach(function(bg){
-                    if(bg.offsetTop < (window.innerHeight + scrollTop)){
-                        bg.style.backgroundImage = "url('" + bg.dataset.src + "')";
-                        bg.classList.remove('lazy-bg');
-                    }
-                });
-
-                if(lazyloadImgs.length==0 && lazyloadBgs.length==0){ 
-                    document.removeEventListener('scroll', lazyload);
-                    window.removeEventListener('resize', lazyload);
-                    window.removeEventListener('orientationChange', lazyload);
+                    img.src = img.dataset.src;
+                    img.classList.remove('lazy-img');
                 }
-            }, 20);
-        }
+            });
+            lazyloadBgs.forEach(function(bg){
+                if(bg.offsetTop < (window.innerHeight + scrollTop)){
+                    bg.style.backgroundImage = "url('" + bg.dataset.src + "')";
+                    bg.classList.remove('lazy-bg');
+                }
+            });
 
-        lazyload();
-        document.addEventListener('scroll', lazyload);
-        window.addEventListener('resize', lazyload);
-        window.addEventListener('orientationChange', lazyload);
+            if(lazyloadImgs.length==0 && lazyloadBgs.length==0){ 
+                document.removeEventListener('scroll', lazyload);
+                window.removeEventListener('resize', lazyload);
+                window.removeEventListener('orientationChange', lazyload);
+            }
+        }, 20);
+    }
 
-    });
-    
+    lazyload();
+    document.addEventListener('scroll', lazyload);
+    window.addEventListener('resize', lazyload);
+    window.addEventListener('orientationChange', lazyload);
+
 });
