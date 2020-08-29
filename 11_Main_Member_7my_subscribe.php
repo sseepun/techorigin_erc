8b5bd0955c278a37aecac8e7eee46a80217007dd
Sarun Seepun <sarun_sla@hotmail.com>
2020-08-29 08:58:41 -0700

    Add 11_Main_Member_7my_subscribe

--- 11_Main_Member_7my_subscribe.php
+++ 11_Main_Member_7my_subscribe.php
@@ -3,48 +3,6 @@
 <head>
     <?php include_once('include/header.php'); ?>
     <?php include_once('include/style.php'); ?>
-    <style>
-        .contact-box .checkbox-container {display:flex; flex-wrap:wrap;}
-        .contact-box .checkbox-container label {padding-left:.25rem; cursor:pointer; }
-        .contact-box .checkbox-container input[type="checkbox"] {display:none;}
-        .contact-box .checkbox-container input[type="checkbox"] + *::before {
-            content: '';
-            display:inline-block;
-            vertical-align:middle;
-            width:.75rem;
-            height:.75rem;
-            border-radius:.25rem;
-            border-style:solid;
-            border-width:.0625rem;
-            border-color:var(--color5);
-            margin-right:.3rem;
-        } 
-        .contact-box .checkbox-container input[type="checkbox"]:checked + *::before {
-            content: '✓';
-            color: #fff;
-            font-size: 50%;
-            line-height: .9375rem;
-            font-weight: 900;
-            text-align:center;
-            background:var(--color4);
-            border-color:var(--color4);
-        }
-        .contact-box .checkbox-container .ss-icon-title label > p {padding-left: 1.475rem;}
-        .contact-box .ss-icon-title .title > p:first-child {font-size: 1.3125rem;}
-        .contact-box .checkbox-container input[type='checkbox']:checked + label::before {}
-        .contact-box .checkbox-container .wrapper {padding-left:3.1875rem;}
-        .contact-box .checkbox-container .grid {position:relative; display:flex; align-items:center;}
-        @media screen and (max-width: 768px) {
-            .contact-box .checkbox-container .wrapper {padding-left:2.6875rem;}
-            .contact-box .checkbox-container .ss-icon-title label > p {margin-top:0;}
-            .ss-icon-title .title > p {margin-top:0;}
-
-        }
-        @media screen and (max-width: 576px) {
-            .contact-box .checkbox-container .wrapper {padding-left:0;}
-            .ss-icon-title .icon  {display:none;}
-        }
-    </style>
 </head>
 <body>
     <?php include_once('include/topnav.php'); ?>
@@ -55,13 +13,13 @@
         ];
         include_once('component/breadcrumb.php');
     ?>
+
     <div class="section-padding">
         <div class="container">
             <div class="ss-header" data-aos="fade-up" data-aos-delay="0">
                 <div class="ss">ข้</div><h1 class="color2">อมูลของฉัน</h1>
                 <p><strong>ระบบจัดการ</strong> เกี่ยวกับการใช้งาน จัดการเนื้อหา และข้อมูล</p>
             </div> 
-
             <div class="grids">
 
                 <div class="grid xl-25 lg-30 sm-100">
@@ -74,7 +32,7 @@
                             </div>
                         </div>
                         <div class="slot-tabs">
-                            <?php for($i=0; $i<4; $i++){?>
+                            <?php for($i=0; $i<5; $i++){?>
                                 <a class="slot-tab <?php if($i==0)echo 'active'; ?>" href="#">
                                     <div class="icon bg-color3 color-white">
                                         <i class="far fa-building"></i>
@@ -90,132 +48,126 @@
                 </div>
 
                 <div class="grid xl-75 lg-70 sm-100">
-                    <div class="contact-box">
-                        <form action="./" method="POST">
-                            <div class="ss-icon-title mt-0 mb-1">
-                                <div class="icon bg-color5"><i class="fas fa-mobile-alt"></i></div>
-                                <h2>แก้ไขติดตามจดหมายข่าวสาร</h2>
-                            </div>
-                            <p>กรุณาเลือกประเภทของข่าวสารที่คุณมีความสนใจในการติดตาม</p>
-                            <div class="grids">
-                                <div class="grid sm-100 mt-0 mb-0-5">
-                                    <div class="field">
-                                        <div class="ss-icon-title mt-0">
-                                            <div class="icon bg-color1"><i class="far fa-building"></i></div>
-                                            <div class="title">
-                                                <p class="fw-600">ข่าวประชาสัมพันธ์</p>
-                                                <p>กรมอนามัย พร้อมให้ข้อมูลข่าวสารที่เป็นประโยชน์สำหรับคุณ</p>
-                                            </div>
-                                        </div>
-                                        <div class="checkbox-container">
-                                            <div class="grids">
-                                                <div class="wrapper">
-                                                    <div class="grid all-100 mt-0-5">
-                                                        <input type="checkbox" id="center" value="value" />
-                                                    <label for="center">ข่าวจากหน่วยงานส่วนกลาง</label>
-                                                    </div>
-                                                    <div class="grid all-100 mt-0-5">
-                                                        <input type="checkbox" id="anamai">
-                                                        <label for="anamai">ข่าวจากศูนย์อนามัย</label>
-                                                    </div>
-                                                    <div class="grid all-100 mt-0-5">
-                                                        <input type="checkbox" id="newspaper">
-                                                        <label for="newspaper">ข่าวจากหนังสือพิมพ์</label>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="grid sm-100 mt-0 mb-0-5">
-                                    <div class="field">
-                                        <div class="checkbox-container">
-                                            <div class="grids">
-                                                <div class="grid all-100 mt-0">
-                                                    <div class="ss-icon-title mt-0">
-                                                        <div class="icon bg-color2"><i class="far fa-building"></i></div>
-                                                        <input type="checkbox" id="่journal" value="value" />
-                                                        <label for="่journal">
-                                                            <span class="fw-600">วารสาร</span>
-                                                            <p>กรมอนามัย ส่งมอบความรู้ที่ถูกต้องเหมาะสมในวิถีชีวิตสู่สุขภาพดี</p>
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="grid sm-100 mt-0 mb-0-5">
-                                    <div class="field">
-                                        <div class="checkbox-container">
-                                            <div class="grids">
-                                                <div class="grid all-100 mt-0">
-                                                    <div class="ss-icon-title mt-0">
-                                                        <div class="icon bg-color3"><i class="far fa-building"></i></div>
-                                                        <input type="checkbox" id="gallery" value="value" />
-                                                        <label for="gallery">
-                                                            <span class="fw-600">คลังภาพ</span>
-                                                            <p>เผยแพร่ความรู้ และวิชาการด้านส่งเสริมสุขภาพและอนามัยสิ่งแวดล้อม</p>
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="grid sm-100 mt-0 mb-0-5">
-                                    <div class="field">
-                                        <div class="checkbox-container">
-                                            <div class="grids">
-                                                <div class="grid all-100 mt-0">
-                                                    <div class="ss-icon-title mt-0">
-                                                        <div class="icon bg-color5"><i class="far fa-building"></i></div>
-                                                        <input type="checkbox" id="video" value="value" />
-                                                        <label for="video">
-                                                            <span class="fw-600">วิดิโอ</span>
-                                                            <p>กรมอนามัย อัพเดท รวบรวมสื่อมัลติมีเดียเพื่อประชาชน</p>
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="grid sm-100 mt-0">
-                                    <div class="field">
-                                        <div class="checkbox-container">
-                                            <div class="grids">
-                                                <div class="grid all-100 mt-0">
-                                                    <div class="ss-icon-title mt-0">
-                                                        <div class="icon bg-color0"><i class="far fa-building"></i></div>
-                                                        <input type="checkbox" id="document" value="value" />
-                                                        <label for="document">
-                                                            <span class="fw-600">ไฟล์เอกสาร</span>
-                                                            <p>กรมอนามัย กับคลังความรู้สุขภาพเพื่อทุกคน</p>
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                
-                            </div>
-                            <div class="btns mt-2">
-                                <button type="submit" class="btn btn-action btn-secondary next-icon">
-                                    อัพเดตข้อมูล
-                                </button>
-                                <a class="btn btn-action btn-primary" href="#">
-                                    ล้างข้อมูล
-                                </a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+                    <form action="./" method="POST">
+                        <div class="ss-icon-title mt-0 mb-2">
+                            <div class="icon bg-color3"><i class="fas fa-mobile-alt"></i></div>
+                            <h2>แก้ไขติดตามจดหมายข่าวสาร</h2>
+                        </div>
+                        <p>กรุณาเลือกประเภทของข่าวสารที่คุณมีความสนใจในการติดตาม</p>
 
+                        <div class="border-bottom pt-1 pb-1">
+                            <div class="ss-icon-title mt-0 mb-0 ai-start">
+                                <div class="icon bg-color4"><i class="fas fa-mobile-alt"></i></div>
+                                <div class="text-wrapper">
+                                    <h4 class="mt-0 fw-400">ข่าวประชาสัมพันธ์</h4>
+                                    <p class="mt-0">
+                                        กรมอนามัย พร้อมให้ข้อมูลข่าวสารที่เป็นประโยชน์สำหรับคุณ
+                                    </p>
+                                    <?php for($i=0; $i<4; $i++){?>
+                                        <div class="field mt-0-5">
+                                            <div class="control checkbox-control">
+                                                <input type="checkbox" name="types[]" id="type_1_<?php echo $i; ?>" value="<?php echo $i; ?>">
+                                                <label for="type_1_<?php echo $i; ?>">
+                                                    ข่าวจากหน่วยงานส่วนกลาง
+                                                </label>
+                                            </div>
+                                        </div>
+                                    <?php }?>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="border-bottom pt-1 pb-1">
+                            <div class="ss-icon-title mt-0 mb-0 ai-start">
+                                <div class="icon bg-color1"><i class="fas fa-mobile-alt"></i></div>
+                                <div class="text-wrapper">
+                                    <div class="field mt-0">
+                                        <div class="control checkbox-control">
+                                            <input type="checkbox" name="types[]" id="type_2" value="2">
+                                            <label for="type_2">
+                                                <h4 class="mt-0 fw-400">วารสาร</h4>
+                                                <p class="mt-0">
+                                                    กรมอนามัย ส่งมอบความรู้ที่ถูกต้องเหมาะสมในวิถีชีวิตสู่สุขภาพดี
+                                                </p>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="border-bottom pt-1 pb-1">
+                            <div class="ss-icon-title mt-0 mb-0 ai-start">
+                                <div class="icon bg-color5"><i class="fas fa-mobile-alt"></i></div>
+                                <div class="text-wrapper">
+                                    <div class="field mt-0">
+                                        <div class="control checkbox-control">
+                                            <input type="checkbox" name="types[]" id="type_3" value="3">
+                                            <label for="type_3">
+                                                <h4 class="mt-0 fw-400">คลังภาพ</h4>
+                                                <p class="mt-0">
+                                                    เผยแพร่ความรู้ และวิชาการด้านส่งเสริมสุขภาพและอนามัยสิ่งแวดล้อม
+                                                </p>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="border-bottom pt-1 pb-1">
+                            <div class="ss-icon-title mt-0 mb-0 ai-start">
+                                <div class="icon bg-color3"><i class="fas fa-mobile-alt"></i></div>
+                                <div class="text-wrapper">
+                                    <div class="field mt-0">
+                                        <div class="control checkbox-control">
+                                            <input type="checkbox" name="types[]" id="type_4" value="4">
+                                            <label for="type_4">
+                                                <h4 class="mt-0 fw-400">วิดิโอ</h4>
+                                                <p class="mt-0">
+                                                    กรมอนามัย อัพเดท รวบรวมสื่อมัลติมีเดียเพื่อประชาชน
+                                                </p>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="pt-1 pb-1">
+                            <div class="ss-icon-title mt-0 mb-0 ai-start">
+                                <div class="icon bg-color0"><i class="fas fa-mobile-alt"></i></div>
+                                <div class="text-wrapper">
+                                    <div class="field mt-0">
+                                        <div class="control checkbox-control">
+                                            <input type="checkbox" name="types[]" id="type_5" value="5">
+                                            <label for="type_5">
+                                                <h4 class="mt-0 fw-400">ไฟล์เอกสาร</h4>
+                                                <p class="mt-0">
+                                                    กรมอนามัย กับคลังความรู้สุขภาพเพื่อทุกคน
+                                                </p>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="btns mt-2">
+                            <button type="submit" class="btn btn-action btn-secondary next-icon">
+                                อัพเดตข้อมูล
+                            </button>
+                            <a class="btn btn-action btn-primary" href="#">
+                                ล้างข้อมูล
+                            </a>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
         </div>
     </div>
+
     <?php include_once('include/footer.php'); ?>
     <?php include_once('include/script.php'); ?>
 </body>
