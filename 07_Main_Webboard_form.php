cbe3faa48c4c2e660520984b30ca3ad2c30f4c52
Sarun Seepun <sarun_sla@hotmail.com>
2020-08-30 10:40:55 -0700

    Complete 07_Main_Webboard_form

--- 07_Main_Webboard_form.php
+++ 07_Main_Webboard_form.php
@@ -3,6 +3,17 @@
 <head>
     <?php include_once('include/header.php'); ?>
     <?php include_once('include/style.php'); ?>
+<style>
+.field .icon-radio{
+    width:12.5rem; display:flex; align-items:flex-start; padding-right:1rem; margin:.375rem 0;
+}
+.field .icon-radio > .icon{
+    display:block; width:2rem; height:2rem; border-radius:.25rem; line-height:2.25rem;
+    font-size:1rem; color:#fff; text-align:center; margin:0 .5625rem 0 0;
+}
+.field .icon-radio > .icon + *{width:calc(100% - 2.5625rem); margin:.375rem 0 0 0;}
+
+</style>
 </head>
 <body>
     <?php include_once('include/topnav.php'); ?>
@@ -24,11 +35,11 @@
             <div class="grids">
 
                 <div class="grid xl-75 lg-70 sm-100">
-                    <div class="btns">
-                        <a class="btn btn-icon" href="#">
+                    <div class="btns text-right">
+                        <a class="btn btn-icon mr-1" href="#">
                             <i class="fas fa-plus-circle"></i> ตั้งกระทู้
                         </a>
-                        <a class="btn btn-icon" href="#">
+                        <a class="btn btn-icon mr-1" href="#">
                             <i class="fas fa-lock"></i> เข้าสู่ระบบ
                         </a>
                         <a class="btn btn-icon" href="#">
@@ -39,7 +50,7 @@
                     <div class="ss-box">
                         <div class="header bg-color3 color-white">
                             <div class="icon"><i class="fas fa-book"></i></div>
-                            <h2 class="color-white">หัวข้อกระทู้</h2>
+                            <h3 class="color-white">หัวข้อกระทู้</h3>
                         </div>
                         <div class="content">
                             <div class="field mt-0">
@@ -49,11 +60,33 @@
                             </div>
                         </div>
                     </div>
-
+                    <div class="ss-box">
+                        <div class="header bg-color4 color-white">
+                            <div class="icon"><i class="fas fa-book"></i></div>
+                            <h3 class="color-white">หมวดหมู่กระทู้</h3>
+                        </div>
+                        <div class="content">
+                            <div class="field d-flex fw-wrap mt-0">
+                                <?php for($i=0; $i<12; $i++){?>
+                                    <div class="icon-radio">
+                                        <div class="icon bg-color<?php echo $i%5; ?>">
+                                            <i class="fas fa-running"></i>
+                                        </div>
+                                        <div class="control-flex">
+                                            <div class="control radio-control">
+                                                <input type="radio" name="radio" id="radio_<?php echo $i; ?>" value="<?php echo $i; ?>">
+                                                <label for="radio_<?php echo $i; ?>">ข่าวประชาสัมพันธ์</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php }?>
+                            </div>
+                        </div>
+                    </div>
                     <div class="ss-box">
                         <div class="header bg-color1 color-white">
                             <div class="icon"><i class="fas fa-book"></i></div>
-                            <h2 class="color-white">รายละเอียด</h2>
+                            <h3 class="color-white">รายละเอียด</h3>
                         </div>
                         <div class="content">
                             <div class="field mt-0">
@@ -63,11 +96,10 @@
                             </div>
                         </div>
                     </div>
-
                     <div class="ss-box">
                         <div class="header bg-color0 color-white">
                             <div class="icon"><i class="fas fa-book"></i></div>
-                            <h2 class="color-white">แนบไฟล๋</h2>
+                            <h3 class="color-white">แนบไฟล์</h3>
                         </div>
                         <div class="content">
                             <div class="field mt-0">
@@ -78,143 +110,54 @@
                         </div>
                     </div>
 
-                    <form action="./" method="POST">
-                        <div class="ss-icon-title mt-0 mb-2">
-                            <div class="icon bg-color3"><i class="fas fa-mobile-alt"></i></div>
-                            <h2>แบบฟอร์มสมัครรับข่าวสารจากกรมอนามัย</h2>
-                        </div>
-                        <div class="field mt-0 mb-2">
-                            <label for="email">
-                                กรุณากรอกอีเมลที่ต้องการรับข่าวสาร *
-                            </label>
-                            <div class="control">
-                                <input type="email" name="email" id="email" required />
-                            </div>
-                        </div>
-                        <p>กรุณาเลือกประเภทของข่าวสารที่คุณมีความสนใจในการติดตาม</p>
+                    <div class="btns text-center">
+                        <a class="btn btn-action btn-secondary" href="#">
+                            โพสกระทู้
+                        </a>
+                        <a class="btn btn-action btn-primary" href="#">
+                            ยกเลิก
+                        </a>
+                    </div>
 
-                        <div class="border-bottom pt-1 pb-1">
-                            <div class="ss-icon-title mt-0 mb-0 ai-start">
-                                <div class="icon bg-color4"><i class="fas fa-mobile-alt"></i></div>
-                                <div class="text-wrapper">
-                                    <h4 class="mt-0 fw-400">ข่าวประชาสัมพันธ์</h4>
-                                    <p class="mt-0">
-                                        กรมอนามัย พร้อมให้ข้อมูลข่าวสารที่เป็นประโยชน์สำหรับคุณ
-                                    </p>
-                                    <?php for($i=0; $i<4; $i++){?>
-                                        <div class="field mt-0-5">
-                                            <div class="control checkbox-control">
-                                                <input type="checkbox" name="types[]" id="type_1_<?php echo $i; ?>" value="<?php echo $i; ?>">
-                                                <label for="type_1_<?php echo $i; ?>">
-                                                    ข่าวจากหน่วยงานส่วนกลาง
-                                                </label>
-                                            </div>
-                                        </div>
-                                    <?php }?>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div class="border-bottom pt-1 pb-1">
-                            <div class="ss-icon-title mt-0 mb-0 ai-start">
-                                <div class="icon bg-color1"><i class="fas fa-mobile-alt"></i></div>
-                                <div class="text-wrapper">
-                                    <div class="field mt-0">
-                                        <div class="control checkbox-control">
-                                            <input type="checkbox" name="types[]" id="type_2" value="2">
-                                            <label for="type_2">
-                                                <h4 class="mt-0 fw-400">วารสาร</h4>
-                                                <p class="mt-0">
-                                                    กรมอนามัย ส่งมอบความรู้ที่ถูกต้องเหมาะสมในวิถีชีวิตสู่สุขภาพดี
-                                                </p>
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div class="border-bottom pt-1 pb-1">
-                            <div class="ss-icon-title mt-0 mb-0 ai-start">
-                                <div class="icon bg-color5"><i class="fas fa-mobile-alt"></i></div>
-                                <div class="text-wrapper">
-                                    <div class="field mt-0">
-                                        <div class="control checkbox-control">
-                                            <input type="checkbox" name="types[]" id="type_3" value="3">
-                                            <label for="type_3">
-                                                <h4 class="mt-0 fw-400">คลังภาพ</h4>
-                                                <p class="mt-0">
-                                                    เผยแพร่ความรู้ และวิชาการด้านส่งเสริมสุขภาพและอนามัยสิ่งแวดล้อม
-                                                </p>
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div class="border-bottom pt-1 pb-1">
-                            <div class="ss-icon-title mt-0 mb-0 ai-start">
-                                <div class="icon bg-color3"><i class="fas fa-mobile-alt"></i></div>
-                                <div class="text-wrapper">
-                                    <div class="field mt-0">
-                                        <div class="control checkbox-control">
-                                            <input type="checkbox" name="types[]" id="type_4" value="4">
-                                            <label for="type_4">
-                                                <h4 class="mt-0 fw-400">วิดิโอ</h4>
-                                                <p class="mt-0">
-                                                    กรมอนามัย อัพเดท รวบรวมสื่อมัลติมีเดียเพื่อประชาชน
-                                                </p>
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div class="pt-1 pb-1">
-                            <div class="ss-icon-title mt-0 mb-0 ai-start">
-                                <div class="icon bg-color0"><i class="fas fa-mobile-alt"></i></div>
-                                <div class="text-wrapper">
-                                    <div class="field mt-0">
-                                        <div class="control checkbox-control">
-                                            <input type="checkbox" name="types[]" id="type_5" value="5">
-                                            <label for="type_5">
-                                                <h4 class="mt-0 fw-400">ไฟล์เอกสาร</h4>
-                                                <p class="mt-0">
-                                                    กรมอนามัย กับคลังความรู้สุขภาพเพื่อทุกคน
-                                                </p>
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <h3 class="fw-400 mt-3-5">ข้อตกลงในการใช้งานกระดานถาม-ตอบ :</h3>
+                    <p class="mt-0-5 pl-1-5">
+                        ในกรณีต้องการตั้งคำถาม ต้องสมัครเป็นสมาชิกเว็ปไซต์ก่อน และล็อกอินเข้าระบบเพื่อตั้งคำถาม
+                        <br>
+                        ข้อความในถาม-ตอบปัญหาสุขภาพนี้ ถือเป็นความคิดเห็นเฉพาะของผู้ตอบคำถาม 
+                        ไม่สามารถนำข้อมูลคำตอบไปอ้างอิงทางกฎหมายได้
+                    </p>
 
-                        <div class="btns mt-2">
-                            <button type="submit" class="btn btn-action btn-secondary next-icon">
-                                ยืนยันการลงทะเบียน
-                            </button>
-                            <a class="btn btn-action btn-primary" href="#">
-                                ล้างข้อมูล
-                            </a>
-                        </div>
-
-                        <h4 class="mt-3 fw-400">เงื่อนไขการรับจดหมายข่าว</h4>
-                        <p class="mt-0">
-                            เมื่อส่งแบบฟอร์มนี้ จะถือว่าคุณยินยอมที่จะรับการติดต่อสื่อสารทางอิเล็กทรอนิกส์จากรกรมอนามัย
-                        </p>
-                    </form>
+                    <h3 class="fw-400 mt-2">หมายเหตุ :</h3>
+                    <p class="mt-0-5">
+                        <span class="fw-500">เว็บบอร์ดนี้จัดทำขึ้นโดยกรมอนามัย กระทรวงสาธารณะสุข</span> 
+                        มีวัตถุประสงค์เพื่อเผยแพร่ความรด้านสุขภาพสำหรับประชาชนโดยทั่วไปเท่านั้น 
+                        ดังนั้นขอความกรุณากลั่นกรองข้อความที่อาจพาดพิงถึงบุคคลอื่นๆ 
+                        เพื่อให้เว็บบอร์ดนี้เป็นแหล่งข้อมูลที่ดีสำหรับทุกคน เนื้อหาของการตอบคำถามแต่ละข้อเป็น
+                        การตอบเฉพาะปัญหานั้นๆ ไม่สามารถนำไปอ้างอิงเปรียบเทียบกับผู้อื่นที่มีปัญหาเดียวกันได้เสมอไป 
+                        ข้อความในคำถาม เกิดจากการเขียนโดยสาธารณชน และส่งขึ้นมาแบบอัตโนมัติ
+                        <br>
+                        <span class="fw-500">ทางเว็บไซต์กรมอนามัย ไม่ได้เป็นเจ้าของลิขสิทธิ์ของข้อความ</span> 
+                        ทางเว็บไซต์ไม่รับผิดชอบต่อข้อความใดๆ ทั้งสิ้นเพราะไม่สามารถระบุได้ว่าเป็นความจริงหรือชื่อผู้เขียน
+                        ที่ได้เห็นคือชื่อจริง ผู้อ่านจึงควรใช้วิจารณญาณในการกลั่นกรอง
+                        <br><br>
+                        <span class="fw-500">
+                            ถ้าท่านพบเห็นข้อความใดที่ขัดต่อกฎหมายและศีลธรรมหรือเป็นการกลั่นแกล้งเพื่อให้เกิดความเสียหาย 
+                            ต่อบุคคลหรือหน่วยงานใด กรุณาส่ง email มาที่ Webmaster/anamai.moph.go.th หรือ 
+                            กดแจ้งที่ปุ่มแจ้งลบกระทู้ เพื่อให้ทีมงานทราบและทำการลบข้อความนั้นออกจากระบบต่อไป 
+                            ขอขอบคุณทุกท่านที่ช่วยแจ้งเข้ามา
+                        </span>
+                    </p>
                 </div>
 
                 <div class="grid xl-25 lg-30 md-100 sm-100" data-aos="fade-up" data-aos-delay="600">
-                    <div class="slots no-mask lazy-bg" data-src="./assets/img/bg/22.jpg">
-                        <img class="float-img type-10" src="./assets/img/hero/22.png" alt="Slot Float Image 01" />
-                        <div class="slot">
-                            <h2 class="header-text quote-text">
-                                <span class="fw-400">เพราะความห่วงใย</span> <br>
-                                สุขภาพที่ดี คือ <br>
-                                <span class="text-xl fw-400 color4">หน้าที่ของเรา</span>
+                    <div class="slots no-mask lazy-bg" data-src="./assets/img/bg/03.jpg">
+                        <img class="float-img type-11" src="./assets/img/hero/23.png" alt="Slot Float Image 01" />
+                        <div class="slot mt-3-5">
+                            <h2 class="header-text quote-text quote-inline">
+                                <span class="fw-400">กรรมอนามัย</span> <br>
+                                เรามีสาระสุขภาพดีๆ <br>
+                                <span class="text-xl fw-400 color1">ส่งตรงถึงคุณ</span> <br>
+                                <span class="text-xl">ทุกวัน</span>
                             </h2>
                         </div>
                     </div>
