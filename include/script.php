4aa1e45fc460eb190bac4b0483762d94c9e63fec
Sarun Seepun <sarun_sla@hotmail.com>
2020-11-18 09:49:00 -0800

    Add Bootstrap 4

--- include/script.php
+++ include/script.php
@@ -5,11 +5,11 @@
 <script src="assets/lib/simple-calendar/simple-calendar.min.js"></script>
 <script src="assets/lib/fancybox-3.5.7/dist/jquery.fancybox.min.js"></script>
 <script src="assets/lib/dropzone-5.7.0/dist/min/dropzone.min.js"></script>
-
 <script src="assets/lib/vanilla-datepicker/dist/js/datepicker.min.js"></script>
-
 <script src="assets/lib/aos-2.3.1/aos.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/TweenMax.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
 <script src="assets/js/script.js"></script>
 <script src="assets/js/lazy-load.js"></script>
--- include/style.php
+++ include/style.php
@@ -4,7 +4,8 @@
 <link rel="stylesheet" type="text/css" href="assets/lib/aos-2.3.1/aos.min.css" media="screen" />
 <link rel="stylesheet" type="text/css" href="assets/lib/vanilla-datepicker/dist/css/datepicker.min.css" />
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
 
 <link rel="stylesheet" type="text/css" href="assets/css/style.css" media="screen" />
 <link rel="stylesheet" type="text/css" href="assets/css/grids.css" media="screen" />
