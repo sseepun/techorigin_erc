7a1faa20ef0e82a7f90885246d87772343247ad3
Sarun Seepun <sarun_sla@hotmail.com>
2020-11-18 09:45:36 -0800

    Initialize

--- include/script.php
+++ include/script.php
@@ -1,18 +1,15 @@
-<script src="./assets/js/jquery-3.5.1.min.js"></script>
-<script src="./assets/lib/semantic-ui/components/transition.min.js"></script>
-<script src="./assets/lib/semantic-ui/components/dropdown.min.js"></script>
-<script src="./assets/lib/slick-1.8.1/slick/slick.min.js"></script>
-<script src="./assets/lib/simple-calendar/simple-calendar.min.js"></script>
-<script src="./assets/lib/fancybox-3.5.7/dist/jquery.fancybox.min.js"></script>
-<script src="./assets/lib/dropzone-5.7.0/dist/min/dropzone.min.js"></script>
+<script src="assets/js/jquery-3.5.1.min.js"></script>
+<script src="assets/lib/semantic-ui/components/transition.min.js"></script>
+<script src="assets/lib/semantic-ui/components/dropdown.min.js"></script>
+<script src="assets/lib/slick-1.8.1/slick/slick.min.js"></script>
+<script src="assets/lib/simple-calendar/simple-calendar.min.js"></script>
+<script src="assets/lib/fancybox-3.5.7/dist/jquery.fancybox.min.js"></script>
+<script src="assets/lib/dropzone-5.7.0/dist/min/dropzone.min.js"></script>
 
-<!-- <script src="./assets/lib/jquery-ui-1.12.1/jquery-ui.min.js"></script> -->
-<script src="./assets/lib/vanilla-datepicker/dist/js/datepicker.min.js"></script>
+<script src="assets/lib/vanilla-datepicker/dist/js/datepicker.min.js"></script>
 
-<!-- <script src="./assets/lib/aos-2.3.1/aos.min.js"></script> -->
+<script src="assets/lib/aos-2.3.1/aos.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/TweenMax.min.js"></script>
 
-<script src="./assets/js/script.js"></script>
-<script src="./assets/js/lazy-load.js"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.min.js"></script>
+<script src="assets/js/script.js"></script>
+<script src="assets/js/lazy-load.js"></script>
--- include/style.php
+++ include/style.php
@@ -1,35 +1,14 @@
-<style>
-    :root{
-        --color0:#47484d;
-        --color1:#c0ca67;
-        --color2:#1d684a;
-        --color3:#284e51;
-        --color4:#0d838d;
-        --color5:#d7d8d0;
-    }
-</style>
-<link rel="stylesheet" type="text/css" href="./assets/lib/semantic-ui/components/transition.min.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="./assets/lib/semantic-ui/components/dropdown.min.css" media="screen" />
 
-<link rel="stylesheet" type="text/css" href="./assets/fonts/includes.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="assets/lib/slick-1.8.1/slick/slick.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="assets/lib/fancybox-3.5.7/dist/jquery.fancybox.min.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="assets/lib/aos-2.3.1/aos.min.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="assets/lib/vanilla-datepicker/dist/css/datepicker.min.css" />
 
-<link rel="stylesheet" type="text/css" href="./assets/lib/slick-1.8.1/slick/slick.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="./assets/lib/simple-calendar/simple-calendar.min.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="./assets/lib/fancybox-3.5.7/dist/jquery.fancybox.min.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="./assets/lib/dropzone-5.7.0/dist/min/dropzone.min.css" media="screen" />
-<!-- <link rel="stylesheet" type="text/css" href="./assets/lib/aos-2.3.1/aos.min.css" media="screen" /> -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
 
-<!-- <link rel="stylesheet" type="text/css" href="./assets/lib/jquery-ui-1.12.1/jquery-ui.min.css" />
-<link rel="stylesheet" type="text/css" href="./assets/lib/jquery-ui-1.12.1/jquery-ui.theme.min.css" /> -->
-<link rel="stylesheet" type="text/css" href="./assets/lib/vanilla-datepicker/dist/css/datepicker.min.css" />
+<link rel="stylesheet" type="text/css" href="assets/css/style.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="assets/css/grids.css" media="screen" />
 
-<link rel="stylesheet" type="text/css" href="./assets/css/style.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="./assets/css/grids.css" media="screen" />
-
-<link rel="stylesheet" type="text/css" href="./assets/css/navs.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="./assets/css/blocks.css" media="screen" />
-<link rel="stylesheet" type="text/css" id="css-theme" href="./assets/css/themes/0.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="./assets/css/ie-fix.css" media="screen" />
-
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.css" />
+<link rel="stylesheet" type="text/css" href="assets/css/navs.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="assets/css/blocks.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="assets/css/ie-fix.css" media="screen" />
